d11aec4b130b7b67e5f2cd1944be13a0efb430ed
Tom Cooper <tomcooper@tec8.net>
2017-04-02 18:26:35 -0700

    added test for fars_summary_years, added testthat to description file

--- tests/testthat/test_fars.R
+++ tests/testthat/test_fars.R
@@ -1,4 +1,3 @@
-
 #test_that("fars_read loads sample data", {
 #  file <- system.file("tests", "accident_2013.csv.bz2", package = "testpackage")
 #  fars_data <- fars_read(file)
@@ -8,5 +7,10 @@
 test_that("fars_read_years loads sample data", {
   fars_data <- fars_read_years(2013)
   expect_that(nrow(fars_data[[1]]), equals(30202))
+})
+
+test_that("fars_summarize loads sample data", {
+  fars_data <- fars_summarize_years(2013)
+  expect_that(fars_data[[2]][1], equals(2230))
 })
 
